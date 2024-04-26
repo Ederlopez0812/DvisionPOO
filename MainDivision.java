c519ace930739f651c43f37c1b40dbf00e59a9d3
Ederlopez0812 <167149255+Ederlopez0812@users.noreply.github.com>
2024-04-25 19:08:21 -0600

    Update MainDivision.java

--- MainDivision.java
+++ MainDivision.java
@@ -8,8 +8,9 @@ public class MainDivision {
     public static void main(String[] args) {
         // se llama al metodo Scanner
         Scanner n1 = new Scanner(System.in);
+        Division d1 = new Division();
         // se ingresa el mensaje que se le dara al usuario
-
+    
         System.out.println("Ingresa un numero");
         // se almacena el valor del usuario
         int a = n1.nextInt();
@@ -20,6 +21,9 @@ public class MainDivision {
         // se hace la operacion con los datos ingresados por el usuario
         int r = a / b;
         // se imprime el resultado de la operacion y se muestra en pantalla
+        d1.a=a;
+        d1.b=b;
+        d1.dividir(a, b);
         System.out.println("El resultado es:" + r);
 
         // se cierra el metodo mian
